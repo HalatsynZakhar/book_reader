d8941a4a782a88e62ab0b3126d6b9cc3203c3306
halatsynzakhar <halatsynzakhar@gmail.com>
2023-05-24 20:34:44 +0300

    add try-catch

--- main.py
+++ main.py
@@ -34,52 +34,47 @@ while True:
     transParagraph = translator.translate(currentParagraph, dest="ru")
     text_trans = transParagraph.text
 
-    while True:
-        index_sentence = []
-        for i in range(len(currentParagraph)):
-            if currentParagraph[i]=="." or currentParagraph[i]=="?" or currentParagraph[i]=="!":
-                index_sentence.append(i)
 
-        index_sentence_trans = []
-        for i in range(len(text_trans)):
-            if text_trans[i]=="." or text_trans[i]=="?" or text_trans[i]=="!":
-                index_sentence_trans.append(i)
+    index_sentence = []
+    for i in range(len(currentParagraph)):
+        if currentParagraph[i]=="." or currentParagraph[i]=="?" or currentParagraph[i]=="!":
+            index_sentence.append(i)
 
+    index_sentence_trans = []
+    for i in range(len(text_trans)):
+        if text_trans[i]=="." or text_trans[i]=="?" or text_trans[i]=="!":
+            index_sentence_trans.append(i)
 
-        for i in range(len(index_sentence)):
-            if i==0:
-                out_yellow(currentParagraph[0:index_sentence[i]+1])
-                print(currentParagraph[index_sentence[i]+1::], end="")
-            else:
-                print(currentParagraph[0:index_sentence[i-1]+1], end="")
-                out_yellow(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1])
-                print(currentParagraph[index_sentence[i]+1::], end="")
+    for i in range(len(index_sentence)):
+        """Вывод параграфа и перевода, с выделением предложения"""
 
-            print("\n")
+        if i==0:
+            out_yellow(currentParagraph[0:index_sentence[i]+1])
+            print(currentParagraph[index_sentence[i]+1::], end="")
+        else:
+            print(currentParagraph[0:index_sentence[i-1]+1], end="")
+            out_yellow(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1])
+            print(currentParagraph[index_sentence[i]+1::], end="")
 
-            if i==0:
-                out_yellow(text_trans[0:index_sentence_trans[i]+1])
-                print(text_trans[index_sentence_trans[i]+1::], end="")
-            else:
+        print("\n")
+
+        if i==0:
+            out_yellow(text_trans[0:index_sentence_trans[i]+1])
+            print(text_trans[index_sentence_trans[i]+1::], end="")
+        else:
+            try:
                 print(text_trans[0:index_sentence_trans[i-1]+1], end="")
                 out_yellow(text_trans[index_sentence_trans[i-1]+1:index_sentence_trans[i]+1])
                 print(text_trans[index_sentence_trans[i]+1::], end="")
-
-
-            input()
-            os.system('cls||clear')
-            i += 1
+            except:
+                pass
 
         inpExit = input("")
+        os.system('cls||clear')
         if inpExit != "":
-            os.system('cls||clear')
-            break
+            with open('bookmark.txt', 'w') as file:
+                file.write(str(bookmark))
+            exit()
 
-
-    inpExit = input("")
-    if inpExit != "":
-        with open('bookmark.txt', 'w') as file:
-            file.write(str(bookmark))
-        exit()
 
     bookmark += 1
