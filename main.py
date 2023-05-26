9805c5b4af933ae4bc2c783664dc3fb9fff97b63
halatsynzakhar <halatsynzakhar@gmail.com>
2023-05-26 13:51:49 +0300

    fix async2, upgraded split to sentence

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 from time import sleep
 
-#import pyttsx3
+# import pyttsx3
 # simple tts, poor quality
 
 from googletrans import Translator
@@ -16,6 +16,7 @@ from playsound import playsound
 
 import pygame
 
+
 def out_red(text):
     print("\033[31m{}\033[37m".format(text), end="")
 
@@ -28,9 +29,8 @@ def out_blue(text):
     print("\033[34m{}\033[37m".format(text), end="")
 
 
-#engine = pyttsx3.init()
+# engine = pyttsx3.init()
 tts_play = True
-
 
 # чтение файла (указан путь к примеру файлу txt)
 with open('Cambias James. A Darkling Sea - royallib.com.txt', encoding='windows-1251') as f:
@@ -43,7 +43,6 @@ with open('bookmark.txt', encoding='windows-1251') as f:
 
 os.system('cls||clear')
 
-
 while True:
     if bookmark == len(list_paragraph):
         break
@@ -53,36 +52,42 @@ while True:
     transParagraph = translator.translate(currentParagraph, dest="ru")
     text_trans = transParagraph.text
 
-
     index_sentence = []
     for i in range(len(currentParagraph)):
-        if currentParagraph[i]=="." or currentParagraph[i]=="?" or currentParagraph[i]=="!":
+        if currentParagraph[i] == "." or currentParagraph[i] == "?" or currentParagraph[i] == "!":
+            if i >= 2:
+                if currentParagraph[i - 2:i] == "Dr":
+                    """
+                    точка обозначает сокращение, не конец предложения. В переводе данная точка отсутствует.
+                    Из-за этого несовпадение количества предложений и неккоректная синхронизация
+                    """
+                    continue
             index_sentence.append(i)
 
     index_sentence_trans = []
     for i in range(len(text_trans)):
-        if text_trans[i]=="." or text_trans[i]=="?" or text_trans[i]=="!":
+        if text_trans[i] == "." or text_trans[i] == "?" or text_trans[i] == "!":
             index_sentence_trans.append(i)
 
     for i in range(len(index_sentence)):
         """Вывод параграфа и перевода, с выделением предложения"""
 
-        if i==0:
-            out_yellow(currentParagraph[0:index_sentence[i]+1])
-            print(currentParagraph[index_sentence[i]+1::], end="")
+        if i == 0:
+            out_yellow(currentParagraph[0:index_sentence[i] + 1])
+            print(currentParagraph[index_sentence[i] + 1::], end="")
         else:
-            print(currentParagraph[0:index_sentence[i-1]+1], end="")
-            out_yellow(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1])
-            print(currentParagraph[index_sentence[i]+1::], end="")
+            print(currentParagraph[0:index_sentence[i - 1] + 1], end="")
+            out_yellow(currentParagraph[index_sentence[i - 1] + 1:index_sentence[i] + 1])
+            print(currentParagraph[index_sentence[i] + 1::], end="")
 
         print("\n")
 
-        if i==0:
-            out_yellow(text_trans[0:index_sentence_trans[i]+1])
-            print(text_trans[index_sentence_trans[i]+1::], end="")
+        if i == 0:
+            out_yellow(text_trans[0:index_sentence_trans[i] + 1])
+            print(text_trans[index_sentence_trans[i] + 1::], end="")
         else:
             try:
-                text1 = text_trans[0:index_sentence_trans[i-1]+1]
+                text1 = text_trans[0:index_sentence_trans[i - 1] + 1]
                 text2 = text_trans[index_sentence_trans[i - 1] + 1:index_sentence_trans[i] + 1]
                 text3 = text_trans[index_sentence_trans[i] + 1::]
 
@@ -95,14 +100,14 @@ while True:
         print("\n")
 
         if tts_play:
-            if i==0:
-                #engine.say(currentParagraph[0:index_sentence[i]+1])
-                tts = gTTS(currentParagraph[0:index_sentence[i]+1], slow=True)
+            if i == 0:
+                # engine.say(currentParagraph[0:index_sentence[i]+1])
+                tts = gTTS(currentParagraph[0:index_sentence[i] + 1], slow=True)
             else:
-                #engine.say(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1])
-                tts = gTTS(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1], slow=True)
+                # engine.say(currentParagraph[index_sentence[i-1]+1:index_sentence[i]+1])
+                tts = gTTS(currentParagraph[index_sentence[i - 1] + 1:index_sentence[i] + 1], slow=True)
             tts.save('sentence.mp3')
-            #engine.runAndWait()
+            # engine.runAndWait()
 
             pygame.init()
             song = pygame.mixer.Sound('sentence.mp3')
@@ -118,7 +123,7 @@ while True:
             file.write(str(bookmark))
         if inpExit != "":
             try:
-                bookmark+=int(inpExit)
+                bookmark += int(inpExit)
             except:
                 exit()
 
