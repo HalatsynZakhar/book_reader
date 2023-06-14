3aa5a096825d4947c456a9042bd2f357dc63b2d4
halatsynzakhar <halatsynzakhar@gmail.com>
2023-06-14 18:05:39 +0300

    add install package.py

---
+++ install-package.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# список необходимых модулей
+required_modules = ['requests', 'beautifulsoup4', 'nltk', 'googletrans==3.1.0a0', 'gtts', 'python-vlc', 'PyQt5', 'lxml']
+
+# получаем список установленных модулей с помощью команды pip freeze
+installed_modules = subprocess.check_output(['pip', 'freeze']).decode('utf-8').split('\n')
+
+# проверяем каждый необходимый модуль на наличие в списке установленных модулей
+for module in required_modules:
+    if module not in installed_modules:
+        # если модуль не установлен, устанавливаем его с помощью команды pip install
+        subprocess.call(['pip', 'install', module])
